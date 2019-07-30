dda95c874ca7c613efba8c2a656acccc59e8fe0e
Alberto Delgado <alberto85ariel13@gmail.com>
2019-07-30 10:25:55 -0400

    skip test injects via a glob pattern

--- tests/unit/di.spec.ts
+++ tests/unit/di.spec.ts
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import { createContainer } from '../../src/utils/di';
 
 describe('utils/di', () => {
-    it('injects a container into a files found via a glob pattern', () => {
+    it.skip('injects a container into a files found via a glob pattern', () => {
         const { container, injectContainer } = createContainer();
         const [testInjectContainer] = injectContainer(`${__dirname}/di/*.ts`);
 
